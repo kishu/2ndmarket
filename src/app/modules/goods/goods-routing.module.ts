c4c2f96ccb31b51dea4a9475f2fb488795f2f37b
jack <kishu@navercorp.com>
2020-06-20 01:12:16 +0900

    wip

--- src/app/modules/goods/goods-routing.module.ts
+++ src/app/modules/goods/goods-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { GoodsListComponent } from './goods-list/goods-list.component';
-import { GoodsDetailComponent } from '@app/modules/goods/goods-detail/goods-detail.component';
+import { GoodsDetailComponent } from './goods-detail/goods-detail.component';
 import { GoodsWriteComponent } from './goods-write/goods-write.component';
 import { GoodsWrite2Component } from './goods-write2/goods-write2.component';
 
@@ -12,10 +12,11 @@ const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['sign-in']);
   imports: [
     RouterModule.forChild([
       { path: 'goods', component: GoodsListComponent, data: { reuse: true } },
-      { path: 'goods/:goodsId', component: GoodsDetailComponent },
+      { path: 'goods/write', component: GoodsWriteComponent },
       // when use guard, reactive form nested goods-write-component is not working
       { path: 'goods/write2', component: GoodsWrite2Component, /* ...canActivate(redirectUnauthorizedToLogin) */ },
-      { path: 'goods/write', component: GoodsWriteComponent, ...canActivate(redirectUnauthorizedToLogin) },
+      { path: 'goods/:goodsId', component: GoodsDetailComponent }
+
     ])
   ],
   exports: [RouterModule]
