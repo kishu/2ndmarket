48dbe9667370dbcea579681f5afbf4a0021a15a7
th <kishu@navercorp.com>
2020-06-25 20:04:21 +0900

    wip

--- src/app/core/http/profiles.service.spec.ts
+++ src/app/core/http/profiles.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { ProfilesService } from './profiles.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('ProfilesService', () => {
+  let service: ProfilesService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(ProfilesService);
   });
 
   it('should be created', () => {
