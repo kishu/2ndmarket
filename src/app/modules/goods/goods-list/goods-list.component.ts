5d0bef3e3759aef24a8c3c5c620a12b0015f9e32
th <kishu@navercorp.com>
2020-07-07 21:14:54 +0900

    wip

--- src/app/modules/goods/goods-list/goods-list.component.ts
+++ src/app/modules/goods/goods-list/goods-list.component.ts
@@ -1,22 +1,30 @@
-import { map, shareReplay, switchMap, tap } from 'rxjs/operators';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { random } from 'lodash-es';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService, GoodsService, } from '@app/core/http';
-import { Goods } from '@app/core/model';
 
 @Component({
   selector: 'app-goods-list',
   templateUrl: './goods-list.component.html',
   styleUrls: ['./goods-list.component.scss']
 })
-export class GoodsListComponent implements OnInit, OnDestroy {
+export class GoodsListComponent implements OnInit {
   groupId$ = this.activatedRoute.paramMap.pipe(
     map(m => m.get('groupId')),
     shareReplay(1)
   );
 
   goodsList$ = this.groupId$.pipe(
-    switchMap(groupId => this.goodsService.getQueryByGroupId(groupId))
+    switchMap(groupId => this.goodsService.getQueryByGroupId(groupId)),
+    map(goodsList => goodsList.map(goods => {
+      return {
+        ...goods,
+        images: goods.images.length > 2 ?
+          [ goods.images[0], goods.images[random(1, goods.images.length - 1)] ] :
+          goods.images
+      }
+    }))
   );
 
   constructor(
@@ -28,9 +36,6 @@ export class GoodsListComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-  }
-
-  ngOnDestroy() {
   }
 
 }
