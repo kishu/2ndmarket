8852a86c6cd6f1b3f694d3433d7e58f41b14c41a
sogoonii <sogoonii@naver.com>
2020-08-11 18:54:32 +0900

    wip

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { map, shareReplay } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '@app/core/http';
 import { PersistenceService } from '@app/core/persistence';
 
@@ -9,12 +10,15 @@ import { PersistenceService } from '@app/core/persistence';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  // url;
   profileExt$ = this.authService.profileExt$;
   newMessagesCount$ = this.persistenceService.newMessageCount$;
   constructor(
+    public router: Router,
     private authService: AuthService,
     private persistenceService: PersistenceService,
   ) {
+    // this.url = router.url;
   }
 
   ngOnInit(): void {
