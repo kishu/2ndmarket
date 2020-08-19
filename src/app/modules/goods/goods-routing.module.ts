8ed9e7bb9110cf4a0be312b74cfaf15e3d71c1c9
jack <kishu@navercorp.com>
2020-08-19 20:40:13 +0900

    wip

--- src/app/modules/goods/goods-routing.module.ts
+++ src/app/modules/goods/goods-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { PermitGoodsGuard } from './permit-goods.guard';
 import { GoodsListComponent } from './goods-list/goods-list.component';
 import { GoodsDetailComponent } from './goods-detail/goods-detail.component';
 import { GoodsWriteComponent } from './goods-write/goods-write.component';
 import { GoodsEditComponent } from './goods-edit/goods-edit.component';
-const redirectUnauthorizedToSignIn = () => redirectUnauthorizedTo(['sign-in']);
 
 @NgModule({
   imports: [
@@ -14,7 +12,6 @@ const redirectUnauthorizedToSignIn = () => redirectUnauthorizedTo(['sign-in']);
       {
         path: 'goods/new/edit',
         component: GoodsWriteComponent,
-        ...canActivate(redirectUnauthorizedToSignIn)
       },
       { path: 'goods', component: GoodsListComponent },
       { path: 'goods/:goodsId', component: GoodsDetailComponent },
