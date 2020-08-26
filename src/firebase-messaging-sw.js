fb65dc46baf750b487f357d9e8412ebf430cedf6
th <kishu@navercorp.com>
2020-08-26 18:48:26 +0900

    wip

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+importScripts('/__/firebase/7.15.0/firebase-app.js');
+importScripts('/__/firebase/7.15.0/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+const messaging = firebase.messaging();
