d881205da188b1e8ac0eaee87ef6b4538c9e4e4b
jack <kishu@navercorp.com>
2020-09-12 17:02:28 +0900

    wip

---
+++ src/app/modules/components/index.ts
@@ -0,0 +1,4 @@
+export * from './cover/cover.component';
+export * from './header/header.component';
+export * from './images-control/images-control.component';
+export * from './limit-timer/limit-timer.component';
