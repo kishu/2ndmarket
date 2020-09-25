4523393a2694e0880831cee4db467e64bd774b4f
jack <kishu@navercorp.com>
2020-09-26 00:03:25 +0900

    wip

--- src/app/core/model/user.ts
+++ src/app/core/model/user.ts
@@ -1,3 +1,7 @@
+import { firestore } from 'firebase';
+import { Group } from '@app/core/model/group';
+import { Profile } from '@app/core/model/profile';
+
 export interface User {
   id: string;
   email: string;
@@ -9,3 +13,23 @@ export interface UserInfo {
   id: string; // userId
   profileId: string;
 }
+
+export interface User2 {
+  id: string;
+  uid: string; // sns login id
+  groupId: string;
+  primary: boolean;
+  activated: boolean;
+  authKey: string;
+  displayName: string;
+  photoURL: string;
+  created: firestore.Timestamp;
+}
+
+export interface User2Ext extends User2 {
+  group: Group;
+}
+
+export type NewUser2 = Omit<Profile, 'id' | 'created'> & {
+  created: firestore.FieldValue;
+};
